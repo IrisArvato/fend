5b195a6f454fb57aa7eb1170355351916fa023b4
tani013 <iris.tan@bertelsmann.de>
2023-07-09 22:32:41 +0800

    weather journal app

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,56 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+app.listen(port, listening);
+
+function listening() {
+    console.log('Server is running at http://localhost:' + port + '...');
+}
+
+// Get Route
+app.get('/all', function (req, res) {
+    if (projectData.zip == undefined) {
+        res.status(204).send({
+            message: "No content"
+        });
+        return;
+    }
+
+    res.status(200).send(projectData);
+});
+
+// Post Route
+app.post('/add', function (req, res) {
+    console.log(req.body); 
+    projectData = {
+        date : req.body.date,
+        temp : req.body.temp,
+        content : req.body.content,
+        zip : req.body.zip,
+    };
+
+    res.status(201).send({
+        message: "Data saved successfully."
+    });
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,96 @@
 /* Global Variables */
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '45d9c70b158b7f87907303efc7472974';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Add event listerner on submit button
+const submitButton = document.getElementById("generate");
+submitButton.addEventListener('click', generateData);
+
+/* Function called by event listener */
+
+// To retrieve the last saved result
+function generateData() {
+    const zip = document.getElementById("zip").value;
+    const content = document.getElementById("feelings").value;
+
+    // Get WeatherData with promise
+    getWeatherData(zip).then((data) => {
+        if (data) {
+            const mappedData = {
+                date: newDate,
+                temp: data.main.temp,
+                content: content,
+                zip: zip
+            };
+
+            postData("/add", mappedData);
+            reloadUi();
+        }
+    });
+};
+
+/* Function to GET Web API Data*/
+const getWeatherData = async (zip) => {
+    try {
+        const param = '&appid=' + apiKey + '&units=imperial';
+        const res = await fetch(baseUrl + zip + param);
+        const data = await res.json();
+
+        return data;
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        console.log('Info', newData.message);
+        return newData;
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
+
+/* Update UI */
+const reloadUi = async () => {
+    const res = await fetch('/all');
+    try {        
+        if (res.status == 204) 
+        {
+            document.getElementById("content").innerHTML = "No recent entry";
+            return;
+        }
+
+        const result = await res.json();
+        console.log(result);
+        
+        document.getElementById("zip").value = result.zip;
+        document.getElementById("feelings").value = result.content;
+
+        document.getElementById("date").innerHTML = "Date: " + result.date;
+        document.getElementById("temp").innerHTML = "Temperature: " + result.temp + '&degF';
+        document.getElementById("content").innerHTML = "Your Feeling: " + result.content;
+    } catch (error) {
+        console.log("Error", error);
+    }
+};
+
+// Start loading Ui with the last saved data
+reloadUi();
